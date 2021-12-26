377b73a3bb0fea52e65fc4500fd02ae3d6b9d43a
Manu Anish <manusanish@gmail.com>
2021-12-26 14:46:16 +0530

    Updated `src`

--- src/server.js
+++ src/server.js
@@ -9,19 +9,23 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 const path = require('path');
 const fs = require('fs');
+const nodeCleanup = require('node-cleanup');
 
 
 // set static files
 app.use(express.static('src/public'));
 
+fs.writeFileSync(path.join(__dirname, './server.bon5r'), "\{ \"online\" : \"true\" \}");
+
 
 // room variables
 var clientCount = 0;
-var roomPassword = "room1234";
-var roomName = "Manu's Room";
-var userList = {
-
-}
+var roomPassword = JSON.parse(fs.readFileSync(path.join(__dirname, './server-data.json')))['room-password'];
+var roomName = JSON.parse(fs.readFileSync(path.join(__dirname, './server-data.json')))['room-name'];
+var isOnline = JSON.parse(fs.readFileSync(path.join(__dirname, './server.bon5r')))['online'];
+// console.log(roomPassword);
+// console.log(roomName);
+// console.log(isOnline);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
@@ -29,21 +33,6 @@ app.get('/', (req, res) => {
 
 http.listen(port, () => {
   console.log(`Bon5R server running at http://localhost:${port}/`);
-});
-
-io.on("connection", (socket) => {
-  clientCount++;
-  socket.emit('client_count_update', clientCount);
-
-  socket.on('disconnect', () => {
-    clientCount--;
-    socket.emit('client_count_update', clientCount);
-  });
-
-  socket.on("decrease_client_count", () => {
-    clientCount--;
-    socket.emit('client_count_update', clientCount);
-  });
 });
 
 io.on("connection", (socket) => {
@@ -58,6 +47,10 @@ io.on("connection", (socket) => {
   socket.on('chat_message', (usr_name, msg, msg_time) => {
     socket.broadcast.emit('chat_message', usr_name, msg, msg_time);
   });
+
+  socket.on("client_count_update", (client_count) => {
+    document.getElementById('client_count').textContent = client_count;
+  });
 });
 
 io.on("connection", (socket) => {
@@ -65,4 +58,8 @@ io.on("connection", (socket) => {
     socket.broadcast.emit('add_user_to_list', usr_name, socket.id);
     console.log("ok");
   });
+});
+
+nodeCleanup((exitCode, signal) => {
+  fs.writeFileSync(path.join(__dirname, './server.bon5r'), "\{ \"online\" : \"false\" \}");
 });
